b61e5fc1301c6f647647ee4ec0aa9fbc40f22757
Suraj Kumar <kumar349118@gmail.com>
2026-06-08 13:16:29 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,9 @@ app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
+    const connectionDb = await mongoose.connect(
+  "mongodb+srv://Surajiitp:Surajiitp123@cluster0.guqp5kl.mongodb.net/?appName=Cluster0"
+)
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
